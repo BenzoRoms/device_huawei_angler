50946cd39fca1ac610928576f8838bf8e4f64467
mydongistiny <jaysonedson@gmail.com>
2017-04-18 11:09:03 -0700

    angler: bring in init and configs for benzoCore
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- init.angler.power.sh
+++ init.angler.power.sh
@@ -19,7 +19,6 @@ function get-set-forall() {
 }
 
 ################################################################################
-
 # disable thermal bcl hotplug to switch governor
 write /sys/module/msm_thermal/core_control/enabled 0
 get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode disable
@@ -37,44 +36,42 @@ write /sys/devices/system/cpu/cpu5/online 0
 write /sys/devices/system/cpu/cpu6/online 0
 write /sys/devices/system/cpu/cpu7/online 0
 
-# Best effort limiting for first time boot if msm_performance module is absent
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 960000
-
-# Limit A57 max freq from msm_perf module in case CPU 4 is offline
-write /sys/module/msm_performance/parameters/cpu_max_freq "4:960000 5:960000 6:960000 7:960000"
-
-# configure governor settings for little cluster
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
+# LITTLE
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor cultivation
 restorecon -R /sys/devices/system/cpu # must restore after interactive
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 19000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 960000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460000:75 960000:80"
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 40000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 384000
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/above_hispeed_delay 20000
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/fastlane 0
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/go_hispeed_load 99
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/go_lowspeed_load 10
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/hispeed_freq 1555200
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/io_is_busy 0
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/max_freq_hysteresis 80000
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/min_sample_time 40000
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/powersave_bias 1
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/target_loads 90
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/timer_rate 20000
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/timer_rate_screenoff 50000
+write /sys/devices/system/cpu/cpu0/cpufreq/cultivation/timer_slack 80000
 
 # online CPU4
 write /sys/devices/system/cpu/cpu4/online 1
 
-# configure governor settings for big cluster
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive
-restorecon -R /sys/devices/system/cpu # must restore after interactive
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 19000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 633600
+#big.
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor cultivation
+restorecon -R /sys/devices/system/cpu
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/above_hispeed_delay 20000
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/fastlane 0
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/go_hispeed_load 99
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/go_lowspeed_load 10
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/hispeed_freq 1958400
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/io_is_busy 0
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/max_freq_hysteresis 80000
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/min_sample_time 40000
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/powersave_bias 1
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/target_loads 90
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/timer_rate 20000
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/timer_rate_screenoff 50000
+write /sys/devices/system/cpu/cpu4/cpufreq/cultivation/timer_slack 800000
 
 # restore A57's max
 copy /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
@@ -84,11 +81,8 @@ write /sys/devices/system/cpu/cpu5/online 1
 write /sys/devices/system/cpu/cpu6/online 1
 write /sys/devices/system/cpu/cpu7/online 1
 
-# Restore CPU 4 max freq from msm_performance
-write /sys/module/msm_performance/parameters/cpu_max_freq "4:4294967295 5:4294967295 6:4294967295 7:4294967295"
-
 # input boost configuration
-write /sys/module/cpu_boost/parameters/input_boost_freq "0:1344000"
+write /sys/module/cpu_boost/parameters/input_boost_freq
 write /sys/module/cpu_boost/parameters/input_boost_ms 40
 
 # Setting B.L scheduler parameters
